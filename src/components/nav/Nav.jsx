1d385e184b121afa283abd82abc68452411da829
eshaan5 <eshaanbagga@gmail.com>
2022-06-01 21:48:47 +0530

    removed linkedin from navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -15,7 +15,7 @@ const Nav = () => {
       <a href="#about" onClick={() => setActive('#about')} className={active === '#about' ? 'active' : ''}><AiOutlineUser size={28} /></a>
       <a href="#portfolio" onClick={() => setActive('#portfolio')} className={active === '#portfolio' ? 'active' : ''}><FaProjectDiagram size={28} /></a>
       <a href="#skills" onClick={() => setActive('#skills')} className={active === '#skills' ? 'active' : ''}><GiSkills size={28} /></a>
-      <a href="#linkedin" onClick={() => setActive('#linkedin')} className={active === '#linkedin' ? 'active' : ''}><FcTimeline size={28} /></a>
+      {/* <a href="#linkedin" onClick={() => setActive('#linkedin')} className={active === '#linkedin' ? 'active' : ''}><FcTimeline size={28} /></a> */}
       <a href="#contact" onClick={() => setActive('#contact')} className={active === '#contact' ? 'active' : ''}><AiOutlinePhone size={28} /></a>
     </nav>
   )
