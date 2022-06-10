fb213cc990284d21f61def9efd5d9c5c70e78824
eshaan <eshaanbagga@gmail.com>
2022-06-10 21:17:55 +0530

    added CV

--- src/components/header/Links.jsx
+++ src/components/header/Links.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Links = () => {
   return (
     <div className='links'>
-        {/* <a href="" className='btn'>View CV</a> */}
+        <a href="https://drive.google.com/file/d/1AD_NA-7gkRM5ORGdbloqxu8BJ-_n9LSK/view?usp=sharing" className='btn' target='_blank' >View CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Connect!</a>
     </div>
   )
