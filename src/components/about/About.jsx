50601c51325bea80a3bd87a5509089f1e321ab5e
eshaan <eshaanbagga@gmail.com>
2022-11-06 00:15:04 +0530

    corrected the college year

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
         <div>
           <div className="about__content">
             <p>
-              I am a second-year student pursuing B.Tech in the field of Information Technology from Indian Institute of Information Technology Sonepat. <br /> <br /> I'm a tech enthusiast with great interest in Web Development, DSA, Competitive Programming and Open Source. I am always looking to explore exciting opportunities, research about innovative ideas, participate in various hackathons and Open-Source events. <br />
+              I am a third-year student pursuing B.Tech in the field of Information Technology from Indian Institute of Information Technology Sonepat. <br /> <br /> I'm a tech enthusiast with great interest in Web Development, DSA, Competitive Programming and Open Source. I am always looking to explore exciting opportunities, research about innovative ideas, participate in various hackathons and Open-Source events. <br />
               <br />I am passionate, self-motivated and an eager learner with a goal-driven attitude. I have strong leadership and communication skills that arise as a result of my deeply empathetic nature. Looking forward to a great opportunity where I can contribute towards upcoming technology innovations and manage key projects that impact the organisation and broaden my scope by learning from the organisations' team members.
             </p>
           </div>
